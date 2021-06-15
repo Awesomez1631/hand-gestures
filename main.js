150ea4490f76e530d83d43556b13de3e04a34b43
Awesomez1631 <72916480+Awesomez1631@users.noreply.github.com>
2021-06-15 18:11:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,54 +1,22 @@
-prediction1 = "";
-prediction2 = "";
-Webcam.set({
-    width : 350,
-    height : 300,
-    image_format : 'png',
-    png_quality : 90
-});
-camera = document.getElementById("camera");
-Webcam.attach('#camera');
-function takesnapshot(){
-    Webcam.snap(function (data_uri){
-    document.getElementById("result").innerHTML = '<img id = "captured_image" src = "' + data_uri + '"/>';
-    });
+function preload(){
+
 }
-console.log('ml5 version :', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/alEugbh5F/model.json', modelLoaded);
-function modelLoaded(){
-    console.log('modelLoaded');
-    
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500);
+    canvas = createCanvas(550,500);
+    canvas.position(560,150);
+    posenet = ml5.poseNet(video,modelloaded);
+    posenet.on('pose', gotPoses);
 }
-function check(){
-    img = document.getElementById('captured_image');
-classifier.classify(img,gotresult);
+function draw(){
+    background('#add8e6');
 }
-/*function speak(){
-    var synth = window.speechSynthesis;
-    speak_data1 = "the first prediction is" + prediction1;
-    speak_data2 = "and the second prediction is" + prediction2;
-    var utterthis = new SpeechSynthesisUtterance (speak_data1 + speak_data2);
-    synth.speak(utterthis);
-}*/
-function gotresult(error,results){
-    if (error){
-        console.error(error);
-    }
-    else {
+function modelloaded(){
+    console.log("POSENET has been initialized");
+}
+function gotPoses(results){
+    if(results.length > 0){
         console.log(results);
-        document.getElementById("result_emotion_name").innerHTML = results[0].label;
-        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
-        prediction1 = results[0].label; 
-        prediction2 = results[1].label;
-        //speak();
-        if (results[0].label == "Gun"){
-            document.getElementById("update_emoji").innerHTML = "&#128073;";
-        }
-        if (results[0].label == "THUMBS UP"){
-            document.getElementById("update_emoji").innerHTML = "&#128077;";
-        }
-        if (results[0].label == "Peace"){
-            document.getElementById("update_emoji").innerHTML = "&#9996;";
-        }
     }
 }
